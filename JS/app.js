0a9790ae2eadca98615c56f0eebec173feebdebf
shahriarlimon <shahriarwebdev@gmail.com>
2022-02-17 13:10:41 +0600

    added total expanse functionality

--- JS/app.js
+++ JS/app.js
@@ -1,35 +1,41 @@
 
 
+function getInputAmount(inputId){
+    const inputValue = document.getElementById(inputId).value;
+    const inputAmount = parseInt(inputValue);
+    return inputAmount;
+}
+
+
+
 document.getElementById("calculate-btn").addEventListener('click',function(){
     // get the input value
-    const incomeTotal = document.getElementById("income-input").value;
-    const incomeAmount = parseInt(incomeTotal);
-    const foodInput = document.getElementById("food-input").value;
-    const foodInputAmount = parseInt(foodInput);
-    const rentInput = document.getElementById("rent-input").value;
-    const rentInputAmount = parseInt(rentInput);
-    const clothInput = document.getElementById("cloth-input").value;
-    const clothInputAmount = parseInt(clothInput);
+    const incomeAmount = getInputAmount("income-input");
+    const foodInputAmount = getInputAmount("food-input")
+    const rentInputAmount = getInputAmount("rent-input");
+    const clothInputAmount = getInputAmount("cloth-input");
 
     // calculating total expanse
+
     const totalExpanse = foodInputAmount + rentInputAmount + clothInputAmount;
     document.getElementById("total-expanse").innerText = totalExpanse;
-
+ 
     // calculating remaining balance
+
     const remainedBalance = incomeAmount - totalExpanse;
     document.getElementById("balance").innerText = remainedBalance;
 });
 
 document.getElementById("save-btn").addEventListener('click',function(){
-    const incomeTotal = document.getElementById("income-input").value;
-    const incomeAmount = parseInt(incomeTotal);
-    const savedInput = document.getElementById("save-input").value;
-    const savedInputNumber = parseFloat(savedInput)/100;
+    const incomeAmount = getInputAmount("income-input");
+    const savedInputAmount = getInputAmount("save-input");
+    const savedInputNumber = savedInputAmount/100;
     // calculating saving amount
     const savedAmount = incomeAmount * savedInputNumber;
     document.getElementById("saving-amount").innerText = savedAmount;
 
-    // calculating overall remaining balance
-    const remainingBalance = incomeAmount - savedAmount;
+    // calculating overall remaining balance after saving
+    const balanceAfterExpanse = document.getElementById("balance").innerText
+    const remainingBalance = balanceAfterExpanse - savedAmount;
     document.getElementById("remaining-balance").innerText = remainingBalance;
 })
